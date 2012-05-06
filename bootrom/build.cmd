8e07e80019e6f150bedd31ad98f5fba2ccbb53b5
Daniel Keep <daniel.keep@gmail.com>
2012-05-07 09:50:10 +1000

    More changes to boot ROM.

--- bootrom/build.cmd
+++ bootrom/build.cmd
@@ -2,11 +2,14 @@
 
 SET SSFS=..\tools\ssfs
 SET KASM=..\tools\kasm --little-8-bit
+SET KDIS=..\tools\kdis --little-8-bit
 SET OUT=bin
 SET RELOC=--relocatable
 
 %KASM% bootrom.dasm -o %OUT%\boot.rom
 IF ERRORLEVEL 1 GOTO STOP
+
+%KDIS% %OUT%\boot.rom -o %OUT%\boot.lst
 
 GOTO EOF
 
