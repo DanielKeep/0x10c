23fffee834eb681595238b293fdbe3feac6ed6f5
Daniel Keep <daniel.keep@gmail.com>
2012-05-09 14:46:08 +1000

    Renamed basicloader.dasm -> loader.dasm.
    Also renamed build script to match.

--- hardos/boot/build.cmd
+++ hardos/boot/build.cmd
@@ -5,5 +5,5 @@ SET KASM=..\..\tools\kasm
 SET KDIS=..\..\tools\kdis
 SET OUT=bin
 
-%KASM% basicloader.dasm screen.dasm utility.dasm ssfs_ro.dasm -o %OUT%\bootload.bin
+%KASM% loader.dasm screen.dasm utility.dasm ssfs_ro.dasm -o %OUT%\bootload.bin
 %KDIS% %OUT%\bootload.bin -o %OUT%\bootload.lst
