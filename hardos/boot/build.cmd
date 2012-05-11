5f00f3edadc767010f887e5ea9488ed8ee4c7879
Daniel Keep <daniel.keep@gmail.com>
2012-05-11 13:51:58 +1000

    Bootloader now disassembled at the correct base address.

--- hardos/boot/build.cmd
+++ hardos/boot/build.cmd
@@ -6,4 +6,4 @@ SET KDIS=..\..\tools\kdis
 SET OUT=bin
 
 %KASM% loader.dasm screen.dasm utility.dasm ssfs_ro.dasm -o %OUT%\bootload.bin
-%KDIS% %OUT%\bootload.bin -o %OUT%\bootload.lst
+%KDIS% %OUT%\bootload.bin -o %OUT%\bootload.lst --base=0x7020
